60051b5b2c5e8b4927afe0dac7c6ae12f4641f69
lyy0709 <2722707908@qq.com>
2024-10-02 21:28:33 +0800

    test

--- assets/nhwwej1nq0zrd7sc.js
+++ assets/nhwwej1nq0zrd7sc.js
@@ -1,2 +1,2 @@
-const c="{{.assetPrefix}}/assets/app-slack-icon-edtfy1gb.png";export{c as S};
+const c="assets/app-slack-icon-edtfy1gb.png";export{c as S};
 //# sourceMappingURL=nhwwej1nq0zrd7sc.js.map
