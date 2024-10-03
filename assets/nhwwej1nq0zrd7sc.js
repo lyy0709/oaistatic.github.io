584af244774317f7b114679e2d98134f05709d82
lyy0709 <2722707908@qq.com>
2024-10-03 13:19:38 +0800

    修复乱码

--- assets/nhwwej1nq0zrd7sc.js
+++ assets/nhwwej1nq0zrd7sc.js
@@ -1,2 +1,2 @@
-const c="/assets/app-slack-icon-edtfy1gb.png";export{c as S};
+const c="https://cdn.closeai.ninja/assets/app-slack-icon-edtfy1gb.png";export{c as S};
 //# sourceMappingURL=nhwwej1nq0zrd7sc.js.map
